587a8d0b7d912313b7cfe67a6ea82d8344f86930
AlexLenk <alexlenk.ru@gmail.com>
2019-12-07 19:57:38 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,117 @@
+var syntax = 'scss'; // Syntax: sass or scss;
+
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync'),
+    rigger = require('gulp-rigger'),
+    uglify = require('gulp-uglify'),
+    fileinclude = require('gulp-ex-file-include'),
+    cleancss = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    autoprefixer = require('gulp-autoprefixer'),
+    notify = require('gulp-notify'),
+    rsync = require('gulp-rsync');
+
+// Local Server
+gulp.task('browser-sync', function () {
+    browserSync({
+        server: {
+            baseDir: './build'
+        },
+        notify: false,
+        // open: false,
+        // online: false, // Work Offline Without Internet Connection
+        // tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+    })
+});
+
+// Sass|Scss Styles
+gulp.task('styles', function () {
+    return gulp.src('./src/' + syntax + '/**/*.' + syntax + '')
+        .pipe(sass({outputStyle: 'expanded'}).on("error", notify.onError()))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer(['last 5 versions']))
+        .pipe(cleancss({level: {1: {specialComments: 0}}})) // Opt., comment out when debugging
+        .pipe(gulp.dest('./build/css'))
+        .pipe(browserSync.stream())
+});
+
+// JS
+gulp.task('scripts', function () {
+    return gulp.src([
+        './src/js/*.js'
+    ])
+        .pipe(rigger())
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(gulp.dest('./build/js'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+// HTML Live Reload
+gulp.task('code', function () {
+    return gulp.src([
+        './src/view/*.html'
+    ])
+        .pipe(fileinclude())
+        .pipe(gulp.dest('./build'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+// Deploy
+gulp.task('rsync', function () {
+    return gulp.src('./src/**')
+        .pipe(rsync({
+            root: './build/',
+            hostname: 'username@yousite.com',
+            destination: 'yousite/public_html/',
+            // include: ['*.htaccess'], // Includes files to deploy
+            exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excludes files from deploy
+            recursive: true,
+            archive: true,
+            silent: false,
+            compress: true
+        }))
+});
+
+gulp.task('img', function () {
+    return gulp.src([
+        './src/img/**/*.*'
+    ])
+        .pipe(gulp.dest('./build/img'))
+        .pipe(browserSync.reload({stream: true}))
+});
+gulp.task('favicon', function () {
+    return gulp.src([
+        './src/favicon/*.*'
+    ])
+        .pipe(gulp.dest('./build/favicon'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+gulp.task('fonts', function () {
+    return gulp.src([
+        './src/fonts/*.*'
+    ])
+        .pipe(gulp.dest('./build/fonts'))
+        .pipe(browserSync.reload({stream: true}))
+});
+
+
+gulp.task('img', gulp.parallel('img'));
+
+gulp.task('favicon', gulp.parallel('favicon'));
+
+gulp.task('fonts', gulp.parallel('fonts'));
+
+gulp.task('watch', function () {
+    gulp.watch('./src/' + syntax + '/**/*.' + syntax + '', gulp.parallel('styles'));
+    gulp.watch(['./src/js/**/*.js'], gulp.parallel('scripts'));
+    gulp.watch('./src/view/*.html', gulp.parallel('code'));
+    gulp.watch('./src/img/**/*', gulp.parallel('img'));
+    gulp.watch('./src/favicon/**/*', gulp.parallel('favicon'));
+    gulp.watch('./src/fonts/**/*', gulp.parallel('fonts'));
+});
+
+gulp.task('default', gulp.parallel('img', 'favicon', 'fonts', 'styles', 'scripts', 'code', 'browser-sync', 'watch'));
---
+++ src/js/lib.js
@@ -0,0 +1,5 @@
+//= ../../node_modules/jquery/dist/jquery.min.js
+//= ../../node_modules/jquery-zoom/jquery.zoom.min.js
+//= ../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
+//= ../../node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js
+//= lib/slick-1.8.1/slick.min.js
