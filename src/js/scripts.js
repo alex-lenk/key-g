aebc2c762567cc28fbeb88abd6db089a3f870c2d
AlexLenk <alexlenk.ru@gmail.com>
2019-12-09 08:46:24 +0300

    Created rating star button

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -85,26 +85,37 @@ $(document).ready(function () {
     /* END Actions on opening menus on mobile devices  */
 
 
+    /* BEGIN Rating star on comment  */
+    $('.js__rating-star__item').click(function () {
+        $('.js__rating-star__item.active').removeClass('active');
+        $(this).toggleClass('active');
+    });
+    /* END Rating star on comment */
+
+
     /* BEGIN Transfer "widget-about" on mobile */
     if ((isMobile.any())) $(".widget-about").appendTo(".single-block__location");
     /* END Transfer "widget-about" on mobile */
 
 
+    /* BEGIN Script scroll to top  */
+    var scrollToTop = $('.scrollToTop');
     $(window).scroll(function () {
         if ($(this).scrollTop() > 250) {
-            $('.scrollToTop').fadeIn();
+            scrollToTop.fadeIn();
         } else {
-            $('.scrollToTop').fadeOut();
+            scrollToTop.fadeOut();
         }
     });
 
     // scroll body to 0px on click
-    $('.scrollToTop').click(function () {
+    scrollToTop.click(function () {
         $('body,html').animate({
             scrollTop: 0
         }, 400);
         return false;
     });
+    /* END Script scroll to top  */
 });
 
 
